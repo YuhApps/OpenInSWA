266ecf5613ca9ac3e92d94dc0bbe2c533f98261d
YuhApps <yuhapps@gmail.com>
2024-03-26 12:24:16 +0700

    Initial Commit

---
+++ OpenInSWA/OpenInSWAApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenInSWAApp.swift
+//  OpenInSWA
+//
+//  Created by George Dao on 26/3/24.
+//
+
+import SwiftUI
+
+@main
+struct OpenInSWAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
